69dc3073383022b99e6afd5289de057ef3a48e33
Job Lampe <job.lampe@student.hu.nl>
2023-01-17 11:57:19 +0100

    update commit

--- train_code/haarCascadeExample.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def generate_negative_description_file():
-
-    with open('neg.txt', 'w') as f:
-        for filename in os.listdir('negative'):
-            f.write('negative/' + filename + '\n')
-
-            # C:/Users/JobMM/openCV/opencv/build/x64/vc15/bin/opencv_annotation.exe --annotations=pos.txt --images=positive/
